59cb0219a6c4588ad7c797a26b7d8d2e0fdc968a
liangzihao <1960916562@qq.com>
2023-07-05 15:39:34 +0800

    feat: 抽屉组件2

--- docs/demos/drawer/drawer-1.vue
+++ docs/demos/drawer/drawer-1.vue
@@ -1,7 +1,60 @@
 <template>
-  <p-drawer></p-drawer>
+    <el-button type="primary" @click="handleDrawerOpen">open</el-button>
+  <p-drawer :drawerObject="drawerObject" @backInnerVisible="backInnerVisible" @backVisible="backVisible" >
+     <template #header>
+            <div>我是头部</div>
+        </template>
+
+        <template #content>
+            <div>我是内容</div>
+            <div>我是内容</div>
+            <div>我是内容</div>
+            <div>我是内容</div>
+            <el-button @click="handleInnerBtn">Click me!</el-button>
+        </template>
+
+        <template #footer>
+            <div style="flex: auto">
+                <el-button @click="cancelClick">取消</el-button>
+                <el-button type="primary" @click="confirmClick">确认</el-button>
+            </div>
+        </template>
+  </p-drawer>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+const drawerObject = reactive({
+  drawerVisible: false,
+  direction: 'rtl', // rtl / ltr / ttb / btt
+  size: '50%',
+  innerDrawer: false,
+  innerDrawerSize: '20%'
+})
+
+const handleDrawerOpen = () => {
+  drawerObject.drawerVisible = true
+}
+
+const cancelClick = () => {
+  drawerObject.drawerVisible = false
+}
+
+const confirmClick = () => {
+  drawerObject.drawerVisible = false
+}
+
+const handleInnerBtn = () => {
+  drawerObject.innerDrawer = true
+}
+
+const backInnerVisible = () => {
+  drawerObject.innerDrawer = false
+}
+
+const backVisible = () => {
+  drawerObject.drawerVisible = false
+}
+</script>
 
 <style scoped lang="scss"></style>
--- packages/drawer/src/drawer.vue
+++ packages/drawer/src/drawer.vue
@@ -1,12 +1,77 @@
 <template>
-  <div>p-drawer</div>
+    <div>
+
+    <el-drawer v-model="drawer" :direction="drawerObject.direction" :size="drawerObject.size" :before-close="handleClose" >
+        <template #header>
+            <slot name="header"></slot>
+        </template>
+        <slot name="content"></slot>
+
+        <el-drawer
+            v-model="innerDrawer"
+            title="I'm inner Drawer"
+            :append-to-body="true"
+            :before-close="handleInnerClose"
+            :size="drawerObject.innerDrawerSize"
+        >
+            <p>_(:зゝ∠)_</p>
+        </el-drawer>
+
+        <template #footer>
+            <slot name="footer"></slot>
+        </template>
+    </el-drawer>
+    </div>
 </template>
 
 <script setup name="p-drawer">
-defineProps({})
-</script>
+import { computed } from 'vue'
 
-<style scoped lang="scss">
-.p-drawer {
+import { ElMessageBox } from 'element-plus'
+const emits = defineEmits(['backInnerVisible', 'backVisible'])
+
+const props = defineProps({
+  drawerObject: {
+    type: Object,
+    default: () => {}
+  }
+})
+
+const drawer = computed(() => {
+  return props.drawerObject.drawerVisible
+})
+
+const innerDrawer = computed(() => {
+  return props.drawerObject.innerDrawer
+})
+
+const handleClose = () => {
+  ElMessageBox.confirm('Are you sure you want to close this?')
+    .then(() => {
+      emits('backVisible', drawer)
+    })
+    .catch(() => {
+      // catch error
+    })
 }
-</style>
+
+const handleInnerClose = () => {
+  ElMessageBox.confirm('Are you sure you want to close this?')
+    .then(() => {
+      emits('backInnerVisible', innerDrawer)
+    })
+    .catch(() => {
+      // catch error
+    })
+}
+
+/**
+    @prop drawerObject | 抽屉配置参数 | Object | {} | {}
+    @prop drawerVisible | 抽屉是否显示 | boolean | true false| false
+    @prop direction | Drawer 打开的方向 | Direction | rtl / ltr / ttb / btt | rtl
+    @prop size | Drawer 窗体的大小 | Drawer 窗体的大小 | - | '30%'
+    @prop innerDrawer | 嵌套抽屉是否显示 | boolean | true / false | false
+    @prop innerDrawerSize | 嵌套Drawer 窗体的大小 | 嵌套Drawer 窗体的大小 | - | '30%'
+ */
+
+</script>
