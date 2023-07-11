7eb8ce4755eec308c881bba98178cf619887e332
pengqianyi <13061951902@163.com>
2023-07-11 16:08:32 +0800

    feat: 提交代码

--- docs/demos/empty/empty-1.vue
+++ docs/demos/empty/empty-1.vue
@@ -1,5 +1,5 @@
 <template>
-  <p-empty :emptyType="emptyType">
+  <p-empty :empty-type="emptyType" empty-image-url="../../demos/empty/images/">
     <el-button type="primary">Button</el-button>
     <!-- <template #image> imgage </template> -->
     <template #description> 提示文字 </template>
@@ -7,7 +7,7 @@
 </template>
 
 <script setup>
-const emptyType = '2'
+const emptyType = '1'
 </script>
 
 <style scoped lang="scss"></style>
